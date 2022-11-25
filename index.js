28f688102bd25d607d7c07f8ea2a35d462c341f9
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-11-26 04:21:04 +0600

    jwt token set up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,17 +12,57 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJwtToken(req, res, next) {
+    const authenticHeader = req.headers.authorization;
+    if (!authenticHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    const token = authenticHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p2sr91x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         const productCategoriesCollections = client.db('Cricket_Lover').collection('categories');
         const productsCollections = client.db('Cricket_Lover').collection('products');
+        const bookingCollections = client.db('Cricket_Lover').collection('bookings');
+        const usersCollections = client.db('Cricket_Lover').collection('users');
 
+
+        //users info
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await usersCollections.updateOne(filter, updateDoc, options);
+            console.log(result);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '7d'
+            });
+            res.send({ result, token });
+        })
 
         app.get('/categories', async (req, res) => {
             const query = {};
             const categories = await productCategoriesCollections.find(query).toArray();
+            res.send(categories);
+        })
+        app.get('/categories/:id', async (req, res) => {
+            const query = req.body.id;
+            const categories = await productsCollections.find(query).toArray();
             res.send(categories);
         })
         app.get('/products', async (req, res) => {
@@ -29,6 +70,11 @@ async function run() {
             const products = await productsCollections.find(query).toArray();
             res.send(products);
         })
+        app.post('/bookings', async (req, res) => {
+            const user = req.body;
+            const result = await bookingCollections.insertOne(user);
+            res.send(result);
+        });
 
     }
     finally {
