bbb32338f3799bf38a1447c826a11b1d546eba58
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-11-28 01:58:53 +0600

    verify & advertise updated api set up

--- index.js
+++ index.js
@@ -49,22 +49,48 @@ async function run() {
                 $set: user
             }
             const result = await usersCollections.updateOne(filter, updateDoc, options);
-            console.log(result);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '7d'
             });
             res.send({ result, token });
         })
+        // app.put('/product/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id)
+        //     const user = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: user
+        //     }
+        //     const result = await usersCollections.updateOne(filter, updateDoc, options);
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '7d'
+        //     });
+        //     res.send({ result, token });
+        // })
+
+        // update for advertise
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    advertise: true
+                }
+            }
+            const result = await productsCollections.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
 
         app.get('/products/:category', async (req, res) => {
             const categoryName = req?.params?.category;
-            console.log('hi category', categoryName)
             const query = {
                 category: categoryName
             }
-            console.log('this is qi', query)
             const products = await productsCollections.find(query).toArray();
-            console.log(products)
             res.send(products);
         })
 
@@ -72,7 +98,6 @@ async function run() {
         app.get('/categories', async (req, res) => {
             const query = {};
             const categories = await productCategoriesCollections.find(query).toArray();
-            console.log(categories)
             res.send(categories);
         })
 
@@ -123,11 +148,23 @@ async function run() {
             res.send(users)
         })
 
+        //one user info
+        app.get('/user', async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
+            const query = { email: email };
+            const user = await usersCollections.findOne(query);
+            res.send(user);
+        });
+
+
         // delete user
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
-            console.log(filter)
             const result = await usersCollections.deleteOne(filter);
             res.send(result)
         });
